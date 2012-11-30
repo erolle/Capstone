7e1be39346307b75610bf87d34e07b6c6c9fcdf4
Erroyl <erroylrolle@hotmail.com>
2012-11-30 18:22:23 -0500

    update start

---
+++ CoadLayout.js
@@ -0,0 +1,18 @@
+//modle
+function stringToNumbers() {
+    
+}
+function
+
+//view
+function drawInnk() {
+    
+}
+
+//controler
+function sendData(){
+    
+}
+function saveToLocalStorage(){
+    
+}
