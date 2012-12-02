2743b8d4288e581e8a1af38f516b34607e4ea7ea
Erroyl <erroylrolle@hotmail.com>
2012-12-02 10:29:04 -0500

    created a working test version

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,36 @@
-function Username(){
-    var username = document.getElementById('username');
+function Username(username){
+    username = username.value;
     if(typeof(Storage)!=="undefined"){
-        localStorage.theUsername=username;
-        document.getElementById("result").innerHTML="Last name: " + localStorage.theUsername;
+    	localStorage.clear();
+        localStorage.Username=username;
     }
-    /*var userArray = new Array();
-    userArray[username] = username;
-    return userArray;*/
-}
-function Pass(valu, id){
-    var userArray
+    show('divUsername', 'hide');
+    show('divPass1','show');
 }
 
-function addToArray(valu){
-    
+function Pass1(valu, num){   
+	divPass='divPass';
+    num2= parseInt(num) + 1;
+    password = valu.value
+    localStorage.Pass1=password;
+    show(divPass + num, 'hide');
+    show(divPass + num2,'show');
+}
+function Pass2(valu, num){   
+	divPass='divPass';
+    num2= parseInt(num) + 1;
+    password = valu.value
+    localStorage.Pass2=password;
+    show(divPass + num, 'hide');
+    show(divPass + num2,'show');
+}
+
+function show(id, visable){
+	document.getElementById(id).className = document.getElementById(id).className.replace( 'hide' , '' );
+	document.getElementById(id).className = document.getElementById(id).className.replace( 'show' , '' );
+	if(visable == "show"){
+		document.getElementById(id).className += "show";
+	}else{
+		document.getElementById(id).className += "hide";
+	}
 }
